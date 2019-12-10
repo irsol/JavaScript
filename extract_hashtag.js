d58cc335562b109b80fb6952dad8d960fa636857
irsol <irinywka@gmail.com>
2019-12-10 21:37:58 +0100

    Fixed extract hashtag solution

--- extract_hashtag.js
+++ extract_hashtag.js
@@ -1,9 +1,22 @@
 // Extract and return a hashtag from string
 
-const hashtagstr = '#cat, dog! #apple, # apple, world#, go#go';
-var regexp = /#(\w+)/g;
-var match = regexp.exec(hashtagstr);
-while (match != null) {
-  console.log(match[1])
-  match = regexp.exec(hashtagstr)
-}
+const hashtagStr = 'dfdfdfd #cat#, dog! #apple, #apple, world#, go#go, #f, #, #1, ###2';
+var regex = /(?:^|\s)(?:#)([a-zA-Z\d]+)/g;
+var matches = [];
+//var match;
+  while ((match = regex.exec(hashtagStr)) !== null) {
+    matches.push(match[1]);  
+  };
+  console.log(matches);
+
+  // for (i = 0, len = hashtagStr.length; i < len; i++) {  
+  //   match = regex.exec(hashtagStr);
+  //   matches.push(match[1]); 
+  //   console.log(matches); 
+  // };
+
+// var match = regexp.exec(hashtagStr);
+// while (match != null) {
+//   console.log(match[1])
+//   match = regexp.exec(hashtagStr)
+// };
